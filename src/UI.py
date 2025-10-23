1fbce186cbf6106f21f63b3a12c915d70c0c0c15
Nick <nickajasahan@gmail.com>
2025-10-23 16:29:56 -0700

    starting re-implementing panning

--- src/UI.py
+++ src/UI.py
@@ -6,10 +6,25 @@ import sys
 class CircuitDesignerView(QGraphicsView):
     def __init__(self, scene):
         super().__init__(scene)
+        self.setRenderHint(QPainter.Antialiasing)
+        self.setDragMode(QGraphicsView.RubberBandDrag)
+        self.zoomFactor = 1.15
+
+    # scrool wheel zoom
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            self.scale(self.zoomFactor, self.zoomFactor)
+        else:
+            self.scale(1/self.zoomFactor, 1/self.zoomFactor)
 
 class CircuitDesignerScene(QGraphicsScene):
     def __init__(self):
         super().__init__()
+        self.panning = False
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MiddleButton:
+            self.panning = True
 
 class CircuitDesignerWindow(QMainWindow):
     def __init__(self):
@@ -41,6 +56,11 @@ class CircuitDesignerWindow(QMainWindow):
         # Main Layout
         mainWidget = QWidget()
         mainLayout = QHBoxLayout()
+        mainLayout.addWidget(sidebar)
+        mainLayout.addWidget(self.view)
+        mainWidget.setLayout(mainLayout)
+
+        self.setCentralWidget(mainWidget)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
